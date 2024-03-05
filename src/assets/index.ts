8c58e5b5f170acad2b5b7d69e825c78c39c8ae0e
shivansh aggarwal <shivneeraj2004@gmail.com>
2024-03-05 20:35:33 +0530

    making spacex website clone

---
+++ src/assets/index.ts
@@ -0,0 +1,12 @@
+import logo from "./asset 0.svg"
+import crew8 from "./crew8_home_6909efcad5.png"
+import transporter10 from "./Transporter_10_SW_Hill_Horiz_0735_Desktop_9cbadd3243.jpg"
+import star6 from "./Star6_41_DSC_4451_desktop_d_6b65017a5f.jpg"
+
+
+export {
+    logo,
+    crew8,
+    transporter10,
+    star6
+}
