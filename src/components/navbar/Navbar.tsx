d482e5f675e8ce250a7fbc0f37315db5af98608a
shivansh <shivneeraj2004@gmail.com>
2024-03-14 23:47:06 +0530

    navbar done

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,26 +1,69 @@
 import { useState } from "react";
 import { logo } from "../../assets";
 import { navbar } from "../../constants";
-import { motion } from "framer-motion"
+import { motion } from "framer-motion";
 
 export default function Navbar() {
+  const [mynav, setMyNav] = useState(navbar);
 
-    const [isHovered, setIsHovered] = useState(true)
-    return (
-        <div className="w-screen h-full">
-            <div className="w-[73.5%] mx-auto flex items-center justify-start py-[2rem]">
+  function handleInHover(id: number) {
+    setMyNav((prev) => {
+      return prev.map((el) => {
+        if (el.id === id) {
+          return { ...el, isHovered: true };
+        } else {
+          return { ...el };
+        }
+      });
+    });
+  }
 
-                <img src={logo} alt="oops" className="w-[210px] h-[26px] mr-[25px] mb-[13px]" />
+  function handleOutHover(id: number) {
+    setMyNav((prev) => {
+      return prev.map((el) => {
+        if (el.id === id) {
+          return { ...el, isHovered: false };
+        } else {
+          return { ...el };
+        }
+      });
+    });
+  }
 
-                <ul className="flex justify-center items-baseline h-full">
-                    {
-                        navbar.map((el) => {
-                            return <div key={el.id} onMouseEnter={() => { setIsHovered(false) }} onMouseLeave={() => { setIsHovered(true) }}><li className=" text-[1rem] uppercase font-semibold text-white leading-[26px] mx-[12px] ">{el.name}</li> <motion.div animate={{ width: isHovered ? "0%" : "100%" }} exit="100%" className="bg-white h-[2px]"></motion.div></div>
-                        })
-                    }
+  return (
+    <div className="w-screen h-full">
+      <div className="w-[73.5%] mx-auto flex items-center justify-start py-[2rem]">
+        <img
+          src={logo}
+          alt="oops"
+          className="w-[210px] h-[26px] mr-[25px] mb-[13px]"
+        />
 
-                </ul>
-            </div>
-        </div>
-    )
+        <ul className="flex justify-center items-baseline h-full">
+          {mynav.map((el) => {
+            return (
+              <div
+                key={el.id}
+                onMouseEnter={() => {
+                  handleInHover(el.id);
+                }}
+                onMouseLeave={() => {
+                  handleOutHover(el.id);
+                }}
+              >
+                <li className=" text-[1rem] uppercase font-semibold text-white leading-[26px] mx-[12px] ">
+                  {el.name}
+                </li>{" "}
+                <motion.div
+                  animate={{ width: !el.isHovered ? "0%" : "100%" }}
+                  exit="100%"
+                  className="bg-white h-[2px]"
+                ></motion.div>
+              </div>
+            );
+          })}
+        </ul>
+      </div>
+    </div>
+  );
 }
--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,41 +1,48 @@
 interface Navbar {
-    id: number,
-    name: string,
+  id: number;
+  name: string;
+  isHovered: boolean;
 }
 
-
-
 export const navbar: Navbar[] = [
-    {
-        id: 1,
-        name: "Falcon 9"
-    },
-    {
-        id: 2,
-        name: "Falcon Heavy"
-    },
-    {
-        id: 3,
-        name: "Dragon"
-    },
-    {
-        id: 4,
-        name: "Starship"
-    },
-    {
-        id: 5,
-        name: "Human Spaceflight"
-    },
-    {
-        id: 6,
-        name: "Rideshare"
-    },
-    {
-        id: 7,
-        name: "Starshield"
-    },
-    {
-        id: 8,
-        name: "Starlink"
-    }
-]
+  {
+    id: 1,
+    name: "Falcon 9",
+    isHovered: false,
+  },
+  {
+    id: 2,
+    name: "Falcon Heavy",
+    isHovered: false,
+  },
+  {
+    id: 3,
+    name: "Dragon",
+    isHovered: false,
+  },
+  {
+    id: 4,
+    name: "Starship",
+    isHovered: false,
+  },
+  {
+    id: 5,
+    name: "Human Spaceflight",
+    isHovered: false,
+  },
+  {
+    id: 6,
+    name: "Rideshare",
+    isHovered: false,
+  },
+  {
+    id: 7,
+    name: "Starshield",
+    isHovered: false,
+  },
+  {
+    id: 8,
+    name: "Starlink",
+    isHovered: false,
+  },
+];
